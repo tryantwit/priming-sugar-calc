178ae40644eda8e265797083a2e0ba5ed6b60104
Tim Ryan <tryant90@gmail.com>
2018-09-15 11:52:02 -0400

    Convert to ounzces as well.

--- priming_sugar/app.js
+++ priming_sugar/app.js
@@ -5,14 +5,19 @@ exports.lambdaHandler = async (event, context) => {
   const volumeCO2 = data.volumeCO2;
   const fermentationTemp = data.fermentationTemp;
 
-  const PrimingSugar = roundToTwo(15.195 * volumeOfBeer * (volumeCO2 - 3.0378 + (0.050062 * fermentationTemp) - (0.00026555 * fermentationTemp ** 2)));
+  const primingInGrams = roundToTwo(15.195 * volumeOfBeer * (volumeCO2 - 3.0378 + (0.050062 * fermentationTemp) - (0.00026555 * fermentationTemp ** 2)));
+  const primingInOz = primingInGrams / 28.34952;
+
 
   try {
     response = {
       'statusCode': 200,
       'body': JSON.stringify({
         'message': {
-          'corn_sugar': PrimingSugar
+          'corn_sugar': {
+            'grams': primingInGrams,
+            'ounces': primingInOz
+          }
         }
       })
     }
