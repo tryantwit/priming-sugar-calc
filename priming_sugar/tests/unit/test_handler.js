76efe142cf89a7852779b0b3e9568274c8f38d3d
Tim Ryan <tryant90@gmail.com>
2019-03-08 19:07:29 -0500

    Fix unit test.

--- priming_sugar/tests/unit/test_handler.js
+++ priming_sugar/tests/unit/test_handler.js
@@ -7,7 +7,7 @@ var event, context;
 
 
 describe('Tests handler', function () {
-  it('verifies successful response', async () => {
+  it('verifies successful response of grams', async () => {
     event = {
       "body": "{\n\t\"volumeOfBeer\": 5,\n\t\"volumeCO2\": 2.5,\n\t\"fermentationTemp\": 65\n}"
     }
@@ -20,6 +20,22 @@ describe('Tests handler', function () {
     let response = JSON.parse(result.body);
 
     expect(response).to.be.an('object');
-    expect(response.message.corn_sugar).to.be.equal(121.13);
+    expect(response.message.corn_sugar.grams).to.be.equal(121.13);
+  });
+
+  it('verifies successful response of ounces', async () => {
+    event = {
+      "body": "{\n\t\"volumeOfBeer\": 5,\n\t\"volumeCO2\": 2.5,\n\t\"fermentationTemp\": 65\n}"
+    }
+
+    const result = await app.lambdaHandler(event, context)
+    expect(result).to.be.an('object');
+    expect(result.statusCode).to.equal(200);
+    expect(result.body).to.be.an('string');
+
+    let response = JSON.parse(result.body);
+
+    expect(response).to.be.an('object');
+    expect(response.message.corn_sugar.ounces).to.be.equal(4.27273548194114);
   });
 });
